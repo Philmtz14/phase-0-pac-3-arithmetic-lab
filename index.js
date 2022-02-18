01cf2451a6dd3cfbabe24044499dcf9630de5a37
philip martinez <martinez.philip00@gmail.com>
2022-02-18 13:20:50 -0600

    commit tree e3ed596f0b1cf2cdd0b4eb0f0b00c707fa7dd3d1

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a,b) {
+return a + b
+}
+function subtract(a,b) {
+    return a - b
+}
+function multiply(a,b) {
+    return a * b
+}
+function divide(a,b) {
+    return a / b
+}
+function increment(a) {
+    return a += 1
+}
+function decrement (a) {
+    return a -= 1
+}
+function makeInt(a = 10) {
+    return a = a
+}
+function makeInt(a = 10) {
+    return parseFloat(a)
+}
+function preserveDecimal(a = 10.50) {
+    return parseFloat(a)
+}
